2f5c49515f0f85ddbff309505b1f5209cd458eac
Meryem Ebru Uyar <eebru.uyarr@gmail.com>
2025-09-22 20:27:43 +0300

    Add files via upload

---
+++ python1question.py
@@ -0,0 +1,13 @@
+"Hello everyone. My name is Ebru. I am a computer engineer student."
+
+4+6
+x=5**2
+print(x)
+y=input("y number:")
+type(y)
+int(y)
+z="hi"
+print(len(z))
+print("my name \t is \nebru")
+
+
