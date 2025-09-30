cf35aa248bd7ecb3adac98c204af5a02ad2c478b
Meryem Ebru Uyar <eebru.uyarr@gmail.com>
2025-09-30 14:50:14 +0300

    Add files via upload

---
+++ Python İF-CONTROLS.py
@@ -0,0 +1,40 @@
+#if-Controls
+
+2>1 and 3<2
+True and False
+
+2>1 or 3<2
+True or False
+
+not 1==1
+10 in [10,20,30]
+10 in {20,30,40}
+
+my_dict={"A":10,"B":11,"C":12,"D":13}
+10 in my_dict.values()
+"B" in my_dict.keys()
+"c" in my_dict.keys()
+
+"ebru" in ("ebru","uyar",2005)
+
+myName="EBRU"
+myName=="EBRU"
+if myName=="EBRU":
+    #identation
+    print("my name is EBRU")
+
+yourAge=input("Enter your age:")
+if yourAge>18:
+    print("you are a young")
+elif yourAge<18:
+    print("you are a child")
+else:
+    print("*_*")
+
+ageList=[11,13,17,18,20]
+"""if x in ageList:
+    print("it has")
+else:
+    print("it haven't")
+"""
+
