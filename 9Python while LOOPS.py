920c217971894822436eb1a897ac9616e7bb36e8
Meryem Ebru Uyar <eebru.uyarr@gmail.com>
2025-10-03 20:45:53 +0300

    Add files via upload

---
+++ 9Python while LOOPS.py
@@ -0,0 +1,19 @@
+#while LOOPS
+
+x=0
+while x<10:
+    print(x)
+    x+=1
+
+p=0
+while p<20:
+    print(f"value of p:{p}")
+    p+=1
+
+my_list=[18,2,5]
+print(f"my list:{my_list}")
+
+name=input("enter name:")
+print(f"welcome:{name}")
+#or
+print("welcome",name)
